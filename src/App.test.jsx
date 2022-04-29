ecbb48b83429b075e0ef70867c094b87ae387f76
abr-ya <gamer05@list.ru>
2022-04-29 22:08:08 +0300

    fix: Fix App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
 import App from './App';
 
-test('renders react on main page', () => {
-  render(<App />);
-  const reactElement = screen.getByText(/react/i);
-  expect(reactElement).toBeInTheDocument();
+describe('With React Testing Library', () => {
+  const initialState = { todos: [] };
+  const mockStore = configureStore();
+  let store;
+
+  test('renders ToDo App header on the page', () => {
+    store = mockStore(initialState);
+    render(<Provider store={store}><App /></Provider>);
+    const reactElement = screen.getByText(/todo list app/i);
+    expect(reactElement).toBeInTheDocument();
+  });
 });
